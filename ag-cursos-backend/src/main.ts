d03a56d8a4e20f08763f73a49e8c57c050cbb029
gustavo-albornoz <gustavo.albornoz88@gmail.com>
2026-06-08 20:15:03 -0300

    fix 8/6 - pt2

--- ag-cursos-backend/src/main.ts
+++ ag-cursos-backend/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL || true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   const port = process.env.PORT || 3000;
   await app.listen(port);
